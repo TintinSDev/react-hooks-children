20b2fadf673e05e6d9ea708d183a74540d99d6c9
Martin <mmaina290@gmail.com>
2023-11-21 20:23:34 +0300

    d

--- src/components/App.js
+++ src/components/App.js
@@ -13,23 +13,27 @@ function App() {
         <div>This is an example!</div>
         <div>This is an example!</div>
       </Container>
-      <Container header="Example header" textPosition="center">
+      <Container header="Turtle head" textPosition="left">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
+        <p>This is an example!</p>
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
+        <p>This is an turtle!</p>
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
+        <p>This is an turtle!</p>
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
+        
       </Container>
     </div>
   );
